f60b53f98d3c340d3a9deb06513678459c496bcd
Le Thanh Trung <trunglt1011@gmail.com>
2023-05-24 00:04:52 +0700

    edit modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,10 +2,18 @@ const viewOrder = document.querySelectorAll(".action .edit-btn");
 const modal = document.querySelector(".modal");
 const modalBody = document.querySelector(".modal-body");
 const closeBtn = document.querySelector(".close-icon");
+const add = document.querySelector(".slidebar .content > a");
 console.log(viewOrder);
+console.log(add);
 
 for (let i = 0; i < viewOrder.length; i++) {
     viewOrder[i].addEventListener("click", () => {
+        modal.classList.add("open");
+    });
+}
+
+if (add) {
+    add.addEventListener("click", () => {
         modal.classList.add("open");
     });
 }
