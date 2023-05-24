06fb386a593962bbaef85f085619878da987c6af
Le Thanh Trung <trunglt1011@gmail.com>
2023-05-25 01:47:08 +0700

    modal gallery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,55 @@
 let inputImg = document.querySelector(".modal .form-group .file");
+let inputGalleryImg = document.querySelectorAll(".gallery .form-group .file");
 let previewImage = document.querySelector(".preview-img");
 console.log(inputImg);
 
-inputImg.addEventListener("change", function (e) {
-    var file = e.target.files[0];
+if (inputImg) {
+    inputImg.addEventListener("change", function (e) {
+        var file = e.target.files[0];
+        console.log(file);
+        // Check if the file is an image
+        if (file && file.type.startsWith("image/")) {
+            // Create a FileReader object
+            var reader = new FileReader();
+            // Set the callback function when the file is loaded
+            reader.onload = function (event) {
+                // Set the source of the preview image
+                previewImage.src = event.target.result;
+            };
+            // Read the file as a data URL
+            reader.readAsDataURL(file);
+        }
+    });
+}
 
-    console.log(file);
-
-    // Check if the file is an image
-    if (file && file.type.startsWith("image/")) {
-        // Create a FileReader object
-        var reader = new FileReader();
-
-        // Set the callback function when the file is loaded
-        reader.onload = function (event) {
-            // Set the source of the preview image
-            previewImage.src = event.target.result;
-        };
-
-        // Read the file as a data URL
-        reader.readAsDataURL(file);
+function getParent(element) {
+    while (element.parentElement) {
+        if (element.parentElement.matches(".form-group")) {
+            return element.parentElement;
+        }
+        element = element.parentElement;
     }
-});
+}
+
+if (inputGalleryImg) {
+    for (let i = 0; i < inputGalleryImg.length; i++) {
+        inputGalleryImg[i].addEventListener("change", function (e) {
+            let previewImg = getParent(inputGalleryImg[i]);
+            console.log(previewImg);
+            var file = e.target.files[0];
+            console.log(file);
+            // Check if the file is an image
+            if (file && file.type.startsWith("image/")) {
+                // Create a FileReader object
+                var reader = new FileReader();
+                // Set the callback function when the file is loaded
+                reader.onload = function (event) {
+                    // Set the source of the preview image
+                    previewImg.querySelector(".preview-img").src = event.target.result;
+                };
+                // Read the file as a data URL
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+}
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,15 +1,29 @@
 const viewOrder = document.querySelectorAll(".action .edit-btn");
+const viewGallery = document.querySelectorAll(".action .gallery-btn");
 const modal = document.querySelector(".modal");
 const modalBody = document.querySelector(".modal-body");
-const closeBtn = document.querySelector(".close-icon");
+const gallery = document.querySelector(".gallery");
+const galleryBody = document.querySelector(".gallery-body");
+const closeModalBtn = document.querySelector(".modal .close-icon");
+const closeGalleryBtn = document.querySelector(".gallery .close-icon");
 const add = document.querySelector(".slidebar .content > a");
 console.log(viewOrder);
 console.log(add);
 
-for (let i = 0; i < viewOrder.length; i++) {
-    viewOrder[i].addEventListener("click", () => {
-        modal.classList.add("open");
-    });
+if (viewOrder) {
+    for (let i = 0; i < viewOrder.length; i++) {
+        viewOrder[i].addEventListener("click", () => {
+            modal.classList.add("open");
+        });
+    }
+}
+
+if (viewGallery) {
+    for (let i = 0; i < viewGallery.length; i++) {
+        viewGallery[i].addEventListener("click", () => {
+            gallery.classList.add("open");
+        });
+    }
 }
 
 if (add) {
@@ -18,7 +32,7 @@ if (add) {
     });
 }
 
-closeBtn.onclick = () => {
+closeModalBtn.onclick = () => {
     hideModal();
 };
 
@@ -32,4 +46,26 @@ modalBody.onclick = (e) => {
 
 function hideModal() {
     modal.classList.remove("open");
+}
+
+if (closeGalleryBtn) {
+    closeGalleryBtn.onclick = () => {
+        hideGallery();
+    };
+}
+
+if (gallery) {
+    gallery.onclick = () => {
+        hideGallery();
+    };
+}
+
+if (galleryBody) {
+    galleryBody.onclick = (e) => {
+        e.stopPropagation();
+    };
+}
+
+function hideGallery() {
+    gallery.classList.remove("open");
 }
